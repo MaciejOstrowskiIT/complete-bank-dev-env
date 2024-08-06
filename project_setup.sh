984295428bbe06012653b35a23e5c4d91b0be344
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2024-08-06 11:27:02 +0200

    changed project_setup.sh behaviour

--- project_setup.sh
+++ project_setup.sh
@@ -16,19 +16,48 @@ DOCKER_SCRIPT="$SCRIPT_DIR/start_docker.sh"
 run_script() {
     local script=$1
     echo "Running $script..." | tee -a "$LOGFILE"
-    if ! bash "$script" >> "$LOGFILE" 2>&1; then
-        echo "Error occurred while running $script. Check the log for details." | tee -a "$LOGFILE"
+    bash "$script" >> "$LOGFILE" 2>&1 &
+    local pid=$!
+
+    # Pasek postępu
+    local delay=0.1
+    local spinstr='|/-\'
+    local temp
+
+    echo -n "Processing $script "
+    local progress_indicator=" [|]"
+
+    while ps -p $pid > /dev/null; do
+        temp=${spinstr#?}
+        progress_indicator=" [${spinstr:0:1}]"
+        spinstr=$temp${spinstr%"$temp"}
+        printf "\rProcessing $script$progress_indicator"
+        sleep $delay
+    done
+    wait $pid
+
+    if [ $? -ne 0 ]; then
+        echo -e "\nError occurred while running $script. Check the log for details." | tee -a "$LOGFILE"
         exit 1
     fi
+
+    printf "\rProcessing $script [Done]\n"
 }
 
+# Liczba skryptów do uruchomienia
+total_scripts=3
+current_script=0
+
 # Uruchom pierwszy skrypt (dodawanie uprawnień)
+current_script=$((current_script + 1))
 run_script "$PERMISSION_SCRIPT"
 
 # Uruchom drugi skrypt (instalacja npm)
+current_script=$((current_script + 1))
 run_script "$NPM_INSTALL_SCRIPT"
 
 # Uruchom skrypt Docker (uruchamianie kontenerów)
+current_script=$((current_script + 1))
 run_script "$DOCKER_SCRIPT"
 
 echo "Project setup completed." | tee -a "$LOGFILE"
